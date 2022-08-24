3e19d0cb96a08692e9a4490a70f240cad2abc257
charlotte <charlottelee849@gmail.com>
2022-08-24 14:53:28 +0800

    update

---
+++ src/components/FAQ.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function FAQ() {
+    return (
+      <>
+        <h2>FAQ</h2>
+        <Link to='/'>Home</Link>
+      </>
+    )
+  }
+
+  export default FAQ;
